10073fc788e81db3dfae9053d2f666fdfa6808ef
Raphael Lima <raphael.aolima8@hotmail.com>
2023-03-24 19:20:29 -0300

    create persistence order context

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Infrastructure.Persistence
+{
+	public class OrderContextSeed
+	{
+		public static async Task SeedAsync(OrderContext orderContext, ILogger<OrderContextSeed> logger)
+		{
+			if (!orderContext.Orders.Any())
+			{
+				orderContext.Orders.AddRange(GetPreconfiguredOrders());
+				await orderContext.SaveChangesAsync();
+				logger.LogInformation("Seed database associated with context {DbContextName}", typeof(OrderContext).Name);
+			}
+		}
+
+		private static IEnumerable<Order> GetPreconfiguredOrders()
+		{
+			return new List<Order>
+			{
+				new Order() { UserName = "swn", FirstName = "Raphael", LastName = "Lima", EmailAddress = "email@email.com", AddressLine = "Teste Rua", Country = "Brasil", TotalPrice = 350 }
+			};
+		}
+
+    }
+}
+
