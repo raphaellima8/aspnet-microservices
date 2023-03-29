fe029555a7047d71203e39cc1adfb0bf04193193
Raphael Lima <raphael.aolima8@hotmail.com>
2023-03-29 20:39:22 -0300

    implementa order repository e mail service

--- src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
+++ src/Services/Ordering/Ordering.Application/Contracts/Persistence/IAsyncRepository.cs
@@ -10,14 +10,14 @@ namespace Ordering.Application.Contracts.Persistence
 		Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>> predicate);
 
 		Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate = null,
-			Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
-			string? includeString = null,
-			bool disableTracking = true);
+										Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+										string? includeString = null,
+										bool disableTracking = true);
 
 		Task<IReadOnlyList<T>> GetAsync(Expression<Func<T, bool>>? predicate = null,
-			Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
-			List<Expression<Func<T, object>>>? includes = null,
-			bool disableTracking = true);
+										Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+										List<Expression<Func<T, object>>>? includes = null,
+										bool disableTracking = true);
 
 		Task<T> GetByIdAsync(int id);
 
