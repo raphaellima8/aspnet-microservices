365a7d785af409ffd7d7c307de1bb8f177f98795
Raphael Lima <raphael.aolima8@hotmail.com>
2023-04-10 21:09:36 -0300

    Configura migração no program.cs

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,7 @@
-﻿using Ordering.Application;
+﻿using Ordering.API.Extensions;
+using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -25,5 +27,13 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app
+    .MigrateDatabase<OrderContext>((context, services) =>
+    {
+        var logger = services.GetService<ILogger<OrderContextSeed>>();
+        OrderContextSeed
+            .SeedAsync(context, logger)
+            .Wait();
+    })
+    .Run();
 
