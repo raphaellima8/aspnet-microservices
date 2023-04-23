d1f4349f885e4f0d90102a50cfe3bb38f0cc6ee7
Raphael Lima <raphael.aolima8@hotmail.com>
2023-04-23 20:47:09 -0300

    Configura Ocelot e adiciona rotas

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,24 @@
+﻿using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.ConfigureLogging(configureLogging =>
+{
+    //configureLogging.ClearProviders();
+    configureLogging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+    configureLogging.AddConsole();
+    configureLogging.AddDebug();
+});
+
+builder.Configuration.AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", true, true);
+
+builder.Services.AddOcelot().AddCacheManager(settings => settings.WithDictionaryHandle());
+
+var app = builder.Build();
+
+app.UseOcelot();
+
+app.Run();
+
